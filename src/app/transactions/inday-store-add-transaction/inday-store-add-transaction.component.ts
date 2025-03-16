2421e711dbe0c471669835f7be4d74a41f62d2e5
Joseph <jowseep@gmail.com>
2025-03-16 22:07:24 +0800

    updated add transaction with reactive forms

--- src/app/transactions/inday-store-add-transaction/inday-store-add-transaction.component.ts
+++ src/app/transactions/inday-store-add-transaction/inday-store-add-transaction.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-inday-store-add-transaction',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './inday-store-add-transaction.component.html',
   styleUrl: './inday-store-add-transaction.component.css'
 })
 export class IndayStoreAddTransactionComponent {
+  addTransactionForm = new FormGroup({
+    borrower: new FormControl(''),
+    item: new FormControl('')
+  });
 
+  submit() {
+    console.log(this.addTransactionForm.value);
+  }
 }
